967bc62bd6452a446ac0e59ef315909b3f773dc1
Julien Nioche <julien@digitalpebble.com>
2019-06-20 11:15:08 +0100

    HostDrivenSpout added payload and getNextHost

--- core/src/main/java/com/digitalpebble/stormcrawler/persistence/HostDrivenSpout.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/persistence/HostDrivenSpout.java
@@ -1,8 +1,27 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.digitalpebble.stormcrawler.persistence;
 
 import java.time.Instant;
+import java.util.LinkedList;
 import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.Optional;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -27,7 +46,9 @@ public abstract class HostDrivenSpout extends AbstractQueryingSpout {
 
     private long lastRefreshTimeHosts = 0;
 
-    private PriorityQueue<HostInfo> hosts;
+    private Queue<HostInfo> hosts = new LinkedList<>();
+
+    private boolean forceRefreshing = false;
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
@@ -38,63 +59,75 @@ public abstract class HostDrivenSpout extends AbstractQueryingSpout {
     }
 
     @Override
-    public void nextTuple() {
-        // check whether the list of hosts should be refreshed
-        boolean needsRefreshing = true;
-        if (lastRefreshTimeHosts != 0) {
-            long difference = System.currentTimeMillis() - lastRefreshTimeHosts;
-            if (difference < frequencyRefreshHosts * 1000) {
-                needsRefreshing = false;
-            }
-        }
-        // needed and not already doing it
-        if (needsRefreshing && !isGettingHosts.get()) {
-            isGettingHosts.set(true);
-            CompletableFuture<PriorityQueue<HostInfo>> futureHosts = refreshHostList();
-            futureHosts.thenAccept(h -> {
-                synchronized (hosts) {
-                    lastRefreshTimeHosts = Instant.now().toEpochMilli();
-                    hosts = h;
-                }
-            });
-        }
+	public void nextTuple() {
+		// check whether the list of hosts should be refreshed
+		boolean needsRefreshing = true;
+		if (lastRefreshTimeHosts != 0) {
+			long difference = System.currentTimeMillis() - lastRefreshTimeHosts;
+			if (difference < frequencyRefreshHosts * 1000) {
+				needsRefreshing = false;
+			}
+		}
 
-        super.nextTuple();
+		if (forceRefreshing) {
+			needsRefreshing = true;
+			forceRefreshing = false;
+		}
+
+		// needed and not already doing it
+		if (needsRefreshing && !isGettingHosts.get()) {
+			isGettingHosts.set(true);
+			CompletableFuture<Queue<HostInfo>> futureHosts = refreshHostList();
+			futureHosts.thenAccept(h -> {
+				synchronized (hosts) {
+					lastRefreshTimeHosts = Instant.now().toEpochMilli();
+					hosts = h;
+				}
+			});
+		}
+
+		super.nextTuple();
+	}
+
+    protected void forceRefreshing() {
+        forceRefreshing = true;
     }
 
-    protected class HostInfo implements Comparable<HostInfo> {
+    protected class HostInfo {
         private final String value;
-        private Instant lastQueried;
+        private Optional<Object> payload;
 
-        HostInfo(String value) {
-            this.value = value;
-            setQueriedNow();
-        }
-
-        public int compareTo(HostInfo o) {
-            // sort first by lastQueries then string value
-            int timecomparison = this.lastQueried.compareTo(o.lastQueried);
-            if (timecomparison != 0)
-                return timecomparison;
-            return value.compareTo(o.value);
+        HostInfo(String key) {
+            this.value = key;
         }
 
         protected String getValue() {
             return this.value;
         }
 
-        protected void setQueriedNow() {
-            lastQueried = Instant.now();
+        public Optional<Object> getPayload() {
+            return payload;
+        }
+
+        public void setPayload(Object payload) {
+            this.payload = Optional.of(payload);
         }
 
     }
 
-    /** @return PriorityQueue of HostInfo to be used by populateBuffer() **/
-    protected PriorityQueue<HostInfo> getHosts() {
-        return new PriorityQueue<HostInfo>(hosts);
+    /** @return next HostInfo to be used by populateBuffer() **/
+    protected HostInfo getNextHost() {
+        HostInfo nextHost = hosts.poll();
+        if (nextHost != null)
+            hosts.offer(nextHost);
+        return nextHost;
     }
 
-    /** Return a priorityqueue of HostInfo to use within populateBuffer() **/
-    protected abstract CompletableFuture<PriorityQueue<HostInfo>> refreshHostList();
+    /**
+     * Return a queue of HostInfo to use within populateBuffer(). The HostInfo
+     * objects will be returned by getNextHost in the order in which they were
+     * provided by this method.
+     **/
+    protected abstract CompletableFuture<Queue<HostInfo>> refreshHostList();
 
 }
